82ba9251bb4c685cd6d5177c87cbd82e15ac41bf
giampieroerrante97-dev <giampieroerrante97@gmail.com>
2025-11-09 13:03:02 +0100

    server.py

--- server.py
+++ server.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Se Render non imposta PORT, usa 5000
+    port = int(os.environ.get("PORT", 10000))  # Se Render non imposta PORT, usa 10000
     app.run(host="0.0.0.0", port=port)
+
 
