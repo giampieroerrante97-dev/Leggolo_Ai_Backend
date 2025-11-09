d1600a40c6f340be1304774c524dbadc4f088816
giampieroerrante97-dev <giampieroerrante97@gmail.com>
2025-11-09 12:40:44 +0100

    server.py

--- server.py
+++ server.py
@@ -31,3 +31,9 @@ def detect_language():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Se Render non imposta PORT, usa 5000
+    app.run(host="0.0.0.0", port=port)
+
